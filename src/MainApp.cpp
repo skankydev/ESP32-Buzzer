a437d59fc1592f1cd3b955dfd7651351d6b05c7b
simon schenck <simon.schenck@yakabiz.com>
2021-10-04 20:07:37 +0200

    des test

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -1,4 +1,5 @@
 #include "MainApp.h"
+#include "BluetoothSerial.h"
 
 /**
  * constructeur
@@ -8,14 +9,19 @@ MainApp::MainApp(){
 	_ledPin = 5;
 	//l entrée analoge pour le nv de baterie
 	_battery = 35;
+	bleuSerial = new BluetoothSerial;
 
 }
 
 void MainApp::init(){
 
 	Serial.begin(38400);
+	bleuSerial->begin("beerover buzzer");
 	pinMode(_ledPin, OUTPUT);
 
+	Serial.println("Coucou");
+	bleuSerial->println("Coucou");
+	digitalWrite(_ledPin, HIGH);
 }
 
 /**
@@ -23,19 +29,27 @@ void MainApp::init(){
  * @return {void} rien
  */
 void MainApp::step(){
-	Serial.println("Coucou");
-	delay(1000);
-
-	digitalWrite(_ledPin, HIGH);
-	Serial.println("On");
-	delay(1000);
-
-	digitalWrite(_ledPin, LOW);
-	Serial.println("Off");
-	delay(1000);
-
+	
 	uint16_t v = analogRead(_battery);
 	float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (1100 / 1000.0);
 	Serial.println("DEBUG: Value: " + String(v) + " ,Battery Voltage is " + String(battery_voltage) + "V");
+	bleuSerial->println("DEBUG: Value: " + String(v) + " ,Battery Voltage is " + String(battery_voltage) + "V");
+	delay(1000);
+	
+	String message = this->readBleu();
+	if(message.length() > 0){
+		Serial.println("Message : "+message);
+		bleuSerial->println("Message : "+message);
+	}
 
+}
+
+
+String MainApp::readBleu(){
+	String message = "";
+	while (bleuSerial->available()) {
+		char input = (char)bleuSerial->read(); 
+		message += String(input);
+	}
+	return message;
 }
--- src/MainApp.h
+++ src/MainApp.h
@@ -3,17 +3,20 @@
 
 
 #include "setting.h"
+#include "BluetoothSerial.h"
 
 class MainApp { //création nouvelle classe MainApp
 
 	private:
 		int _ledPin;
 		int _battery;
+		BluetoothSerial* bleuSerial;
 
 	public:
 		MainApp();
 		void init();
 		void step();
+		String readBleu();
 };
 
 #endif
